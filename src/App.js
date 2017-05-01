b70f2e0f803fec0e5777132f3422113682ab6804
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-01 07:08:26 +0100

    adding reusable component to build Login form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import Provider from 'react-redux';
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+    apiKey: 'AIzaSyCKQnD-_WYt7Zpwgf-xw9xfguOClgWg-6Q',
+    authDomain: 'manager-c09f5.firebaseapp.com',
+    databaseURL: 'https://manager-c09f5.firebaseio.com',
+    projectId: 'manager-c09f5',
+    storageBucket: 'manager-c09f5.appspot.com',
+    messagingSenderId: '1096390103989'
+  };
+
+  firebase.initializeApp(config);
+  }
+
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
-        <View>
-          <Text>Hello</Text>
-        </View>
+        <LoginForm />
       </Provider>
     );
   }
