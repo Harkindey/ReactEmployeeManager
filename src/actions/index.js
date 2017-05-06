7c5591327e4c23b9e0671e5a4e3c572a1d833fd3
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-06 23:34:10 +0100

    Creating user account

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,8 @@ import firebase from 'firebase';
 import {
   EMAIL_CHANGED,
   PASSWORD_CHANGED,
-  LOGIN_USER_SUCCESS
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_FAIL
 } from './types';
 
 export const emailChanged = (text) => {
@@ -23,14 +24,22 @@ export const loginUser = ({ email, password }) => {
     return (dispatch) => {
       console.log('bean');
       firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(user => {
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
-      })
+      .then(user => loginUserSucess(dispatch, user))
       .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(user => {
-          dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
-        });
-      }); 
+        .then(user => loginUserSucess(dispatch, user))
+        .catch(() => loginUserFail(dispatch));
+      });
     };
+};
+
+const loginUserFail = (dispatch, user) {
+  dispatch({type: LOGIN_USER_FAIL});
+};
+
+const loginUserSucess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  });
 };
