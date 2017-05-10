deba30b1b274c85607f8142ac2d4cb2735b65bb9
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-10 22:37:40 +0100

    Pickers and Style Overrides

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Picker } from 'react-native';
+import { Picker, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
@@ -16,7 +16,7 @@ class EmployeeCreate extends Component {
             onChangeText={(value) => this.props.employeeUpdate({ prop: 'name', value })}
           />
         </CardSection>
-        
+
         <CardSection>
           <Input
             label="Phone"
@@ -26,7 +26,8 @@ class EmployeeCreate extends Component {
           />
         </CardSection>
 
-        <CardSection>
+        <CardSection style={{ flexDirection: 'column' }}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
           <Picker
             style={{ flex: 1 }}
             selectedValue={this.props.shift}
@@ -47,10 +48,18 @@ class EmployeeCreate extends Component {
             Create
           </Button>
         </CardSection>
+
       </Card>
     );
   }
 }
+
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+};
 
 const mapStateToProps = (state) => {
   const { name, phone, shift } = state.employeeForm;
