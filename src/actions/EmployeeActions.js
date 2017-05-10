a04f51f5ac1ff3fce92d9d37277ea7bcffe75458
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-10 11:57:41 +0100

    Employee Form Actions

---
+++ src/actions/EmployeeActions.js
@@ -0,0 +1,10 @@
+imnport {
+  EMPLOYEE_UPDATE
+} from './types';
+
+export const employeeUpdate = ({ prop, value }) => {
+  return {
+    type: EMPLOYEE_UPDATE,
+    payload: { prop, value }
+  };
+};
