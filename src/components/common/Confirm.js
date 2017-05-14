5f6ccd4849c94110577918e780c80aa88f050d4f
Emmanuel AKINDE <ibukunbamise@yahoo.com>
2017-05-14 19:37:33 +0100

    Modal as a Resusable Component

---
+++ src/components/common/Confirm.js
@@ -0,0 +1,10 @@
+import React, { Component} from 'react';
+import { Text, View, Modal } from 'react-native';
+import { CardSection, Button } from '../CardSection';
+import { Button } from './Button';
+
+const Confirm = () => {
+  
+};
+
+export { Confirm };
